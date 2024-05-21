ed6e713f853dbea494956aa28e8214590dcdafe9
Fernando Sinisi <fernando.sinisi@despegar.com>
2024-05-21 20:14:46 -0300

    CorsPolicy add cors
    Signed-off-by: Fernando Sinisi <fernando.sinisi@despegar.com>

--- app/index.js
+++ app/index.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const express = require('express')
-
+const cors = require('cors');
 const {ROUTES} = require("./routes");
 
 const {setupLogging} = require("./logging");
@@ -12,6 +12,8 @@ const app = express()
 const port = process.env.PORT;
 
 app.use(express.json())
+app.use(cors());
+
 setupLogging(app);
 setupAuth(app, ROUTES);
 setupProxies(app, ROUTES);
